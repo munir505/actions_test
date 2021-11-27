760f9c7ef2459d8aa963d396bc98ce4492ef168b
Munir Kakar <munir.kakar@hotmail.com>
2021-11-27 15:26:11 +0000

    Adding disk and network

--- main.tf
+++ main.tf
@@ -5,6 +5,23 @@ resource "google_storage_bucket" "gcs_bucket" {
 
 resource "google_compute_instance" "actions_test" {
   name         = "actions-test"
-  machine_type = "n1-standard-1"
   zone         = "europe-west2-c"
+  machine_type = "n1-standard-2"
+
+  network_interface {
+    subnetwork = "projects/ops-munir-kakar/regions/europe-west2/subnetworks/default"
+  }
+
+  boot_disk {
+    auto_delete = "false"
+    source      = google_compute_disk.actions_test_disk.self_link
+  }
+}
+
+resource "google_compute_disk" "actions_test_disk" {
+  name  = "actions-test-disk"
+  type  = "pd-ssd"
+  zone  = "europe-west2-c"
+  size  = 100
+  image = "debian-8-jessie-v20170523"
 }
